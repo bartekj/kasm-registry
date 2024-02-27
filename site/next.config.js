ea0a503f1ce418be116b8d57f87a90392831efe3
Bart <bartekj@users.noreply.github.com>
2024-02-27 11:15:58 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Bartekj Kasm registry',
+    description: 'Couple of custom KASM workspaces',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://bartekj.github.io/kasm-registry/,
+    contactUrl: 'https://github.com/bartekj/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
